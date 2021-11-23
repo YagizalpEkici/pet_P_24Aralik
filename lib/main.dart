b8d0be71afadb95125dfe4fb7a9ea8ee8882e5a3
bpolatCS <bpolat@sabanciuniv.edu>
2021-11-23 22:38:21 +0300

    dfsdfsdf

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp());
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+    ),
+    ),);
+  }
+}
+
+
