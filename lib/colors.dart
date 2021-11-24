3cc9d45609c0fa6572c2f8bb7bfbb7b48dbd7bd3
Bengisu Özdemir <94243264+bengisuozdemir@users.noreply.github.com>
2021-11-24 23:04:51 +0300

    colors and styles

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color Background = Colors.white;
+  static const Color paw_icon = const Color(0XB390DEFF);
+  static const Color text_color = Colors.black;
+  static const Color app_icons = Colors.grey;
+  static const Color click_me_text = Colors.orange;
+}
