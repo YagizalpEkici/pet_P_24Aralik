16c74a72c9c5b5c03acb22db53cdb0018bb9b0d3
esra nur özüm <esraozum@sabanciuniv.edu>
2021-11-25 23:20:27 +0300

    loginpage revised dimensions.dart added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors{
 
+  static const Color login_button_Color = Colors.blueGrey;
   static const Color paw_icon = const Color(0XB390DEFF);
   static const Color text_color = Colors.black;
   static const Color app_icons = Colors.grey;
   static const Color click_me_text = Colors.orange;
+  static const Color login_page_signup = Colors.deepOrangeAccent;
   static const Color wThroughHeadline = Colors.black;
   static const Color Background = Colors.white;
   static const Color appBarColor = const Color(0xFF7FEAD4);
