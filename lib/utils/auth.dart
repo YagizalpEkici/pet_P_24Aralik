d44eaddc8366cc2a1d52fbd4c62cc194de69bfae
mustafaataonbas <aonbas@sabanciuniv.edu>
2021-12-13 00:27:20 +0300

    Google implemented(Unhandled Exception: PlatformException(sign_in_failed, com.google.android.gms.common.api.ApiException: 12500: , null, null))

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -40,6 +42,27 @@ class AuthService {
       return null;
     }
   }
+
+  Future googleSignIn() async{
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    UserCredential result = await FirebaseAuth.instance.signInWithCredential(credential);
+    User? user = result.user;
+    return _userFromFirebase(user);
+  }
+
+
 
   Future signOut() async {
     try {
