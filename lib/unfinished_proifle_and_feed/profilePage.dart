e8940defe7b8b88bf2251366b3af67db3be031c6
bpolatCS <bpolat@sabanciuniv.edu>
2021-12-19 22:11:11 +0300

    sidebar her yere eklendi

--- lib/unfinished_proifle_and_feed/profilePage.dart
+++ lib/unfinished_proifle_and_feed/profilePage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:foldable_sidebar/foldable_sidebar.dart';
 import 'package:pet_project/profileAppBarPages/friendshipRequests.dart';
 import 'package:pet_project/profileAppBarPages/options.dart';
 import 'package:pet_project/unfinished_proifle_and_feed/HomeScreen.dart';
 import 'package:pet_project/profileAppBarPages/editProfile.dart';
+
+import 'custom_sidebar_drawer.dart';
 /*
 void main(){
   runApp(MaterialApp(
@@ -19,6 +22,8 @@ class profilePage extends StatefulWidget {
 }
 
 class _profilePageState extends State<profilePage> {
+
+  FSBStatus _fsbStatus = FSBStatus.FSB_CLOSE;
 
 
   int currentIndex = 0;
@@ -37,9 +42,13 @@ class _profilePageState extends State<profilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.green,
-        leading: IconButton(icon:Icon(Icons.arrow_back),
-            onPressed:() => Navigator.pushNamed(context, '/homePage')
+        leading: IconButton(icon:Icon(Icons.menu),
+            onPressed:() {setState(() {
+            _fsbStatus = _fsbStatus == FSBStatus.FSB_OPEN ?
+            FSBStatus.FSB_CLOSE : FSBStatus.FSB_OPEN;
+            });}//Navigator.pushNamed(context, '/homePage')
         ),
         title: Text(
           'nameOfTheApp',
@@ -63,180 +72,194 @@ class _profilePageState extends State<profilePage> {
 
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+      body: FoldableSidebarBuilder(
+        drawerBackgroundColor: Colors.white,
+        drawer: CustomSidebarDrawer(drawerClose: (){
+          setState(() {
+            _fsbStatus = FSBStatus.FSB_CLOSE;
+          });
+        },
+        ),
+        screenContents: profileScreen(),
+        status: _fsbStatus,
+      ),
+    );
+  }
+
+  Widget profileScreen(){
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
                 ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                Text('Follower',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Chip(
-                              label:
-                              Container(
-                                width: 80,
-                                height: 20,
-                              ),
-                            ),
-                            SizedBox(height: 15,),
-                            Row(
-                              children: [
-                                Text('Breed',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),),
-                              ],
-                            ),
-                            Chip(
-                              label:
-                              Container(
-                                width: 80,
-                                height: 20,
-
-                              ),
-                            ),
-                          ],
-
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(14),
-                          child: CircleAvatar(
-                            radius: 70,
-                            child: ClipOval(
-                                child: Image.network(
-                                    'https://cdn-1.motorsport.com/images/amp/YpN8nVN0/s1000/sergio-perez-red-bull-racing-1.jpg')
-                            ),
-                          ),
-                        ),
-
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                Text('Following',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),),
-                              ],
-                            ),
-                            Chip(
-                              label:
-                              Container(
-                                width: 80,
-                                height: 20,
-                              ),
-                            ),
-                            SizedBox(height: 15,),
-                            Row(
-                              children: [
-                                Text('Age',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),),
-                              ],
-                            ),
-                            Chip(
-                              label:
-                              Container(
-                                width: 80,
-                                height: 20,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Bio',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
-                      child: Card(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),),
-                        color: Colors.grey[300],
-                        elevation: 8,
-                        child: Column(
-                         children: [
-                           Padding(
-                             padding: const EdgeInsets.all(8.0),
-                             child: Text(
-                                 'asjldfnasjofosfnajsfasjbafjjnaskjfnasjbflasjbfasbnfkajsbfkajsbfkjasbfkajsbfkajsbfkjasbfkajsbfkjasbfjaksf'
-                                     'laksfnlasknflkasnflkasnflkanslkfnaslkfnalsknfas'
-                                     'fasknflaksnflkasnflaksnflkasnflnaslfknaslkfnas'
-                                     'fıasnflkansflanslfjnasljfnajslkasldknaşslkfjbljaskdbflkjsabdfjkasbdfasdffajskbf',
-                               style: TextStyle(
-                                 color: Colors.black,
-                                 fontSize: 15,
-                                 fontWeight: FontWeight.w400,
-
-                               ),
-                             ),
-                           ),
-
-                         ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Divider(thickness: 4,
-                    color: Colors.black,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('Follower',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Chip(
+                            label:
+                            Container(
+                              width: 80,
+                              height: 20,
+                            ),
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: [
+                              Text('Breed',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                            ],
+                          ),
+                          Chip(
+                            label:
+                            Container(
+                              width: 80,
+                              height: 20,
+
+                            ),
+                          ),
+                        ],
+
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: CircleAvatar(
+                          radius: 70,
+                          child: ClipOval(
+                              child: Image.network(
+                                  'https://cdn-1.motorsport.com/images/amp/YpN8nVN0/s1000/sergio-perez-red-bull-racing-1.jpg')
+                          ),
+                        ),
+                      ),
+
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text('Following',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                            ],
+                          ),
+                          Chip(
+                            label:
+                            Container(
+                              width: 80,
+                              height: 20,
+                            ),
+                          ),
+                          SizedBox(height: 15,),
+                          Row(
+                            children: [
+                              Text('Age',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                            ],
+                          ),
+                          Chip(
+                            label:
+                            Container(
+                              width: 80,
+                              height: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Bio',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                    child: Card(
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),),
+                      color: Colors.grey[300],
+                      elevation: 8,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'asjldfnasjofosfnajsfasjbafjjnaskjfnasjbflasjbfasbnfkajsbfkajsbfkjasbfkajsbfkajsbfkjasbfkajsbfkjasbfjaksf'
+                                  'laksfnlasknflkasnflkasnflkanslkfnaslkfnalsknfas'
+                                  'fasknflaksnflkasnflaksnflkasnflnaslfknaslkfnas'
+                                  'fıasnflkansflanslfjnasljfnajslkasldknaşslkfjbljaskdbflkjsabdfjkasbdfasdffajskbf',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Column(
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
               children: [
-
+                Divider(thickness: 4,
+                  color: Colors.black,
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+          Column(
+            children: [
+
+            ],
+          ),
+        ],
       ),
     );
   }
